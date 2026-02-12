94a07bf1b231aa1be009ab299942850d0da2a257
Prashant Kumar <prashantkumar11bth-cpu@users.noreply.github.com>
2026-02-12 12:00:00 +0530

    Day 12 - DSA Practice

---
+++ Day_12(12-02-26).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int a[100][100];
+
+    // Read matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check square matrix first
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+    return 0;
+}
