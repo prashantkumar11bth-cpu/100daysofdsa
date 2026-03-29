a63cba7a0ffb6b020059f662d0e5c94cea4f924a
Prashant Kumar <prashantkumar11bth-cpu@users.noreply.github.com>
2026-03-29 12:00:00 +0530

    Day 57 - DSA Practice

---
+++ Day_57(29-03-26).c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void mirror(struct Node* root){
+    if(root == NULL)
+        return;
+
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void inorder(struct Node* root){
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    struct Node* nodes[n];
+
+    for(int i=0;i<n;i++){
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        if(nodes[i] != NULL){
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+
+            if(l < n) nodes[i]->left = nodes[l];
+            if(r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    struct Node* root = nodes[0];
+
+    mirror(root);
+    inorder(root);
+
+    return 0;
+}
